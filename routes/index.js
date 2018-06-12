5c81bcdfb7c1a0b1628e0a35cbf650c63fa7e7b0
jmarticek <marticekjuraj@gmail.com>
2018-06-12 10:20:56 +0200

    add sensor things

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var senzor = require('htu21d');
+var device = "/dev/i2c-1";
+var address = 0x40;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/senzor', function(req, res, next) {
+  
+  var senzor = new senzor.Htu21d(device, address)
+  var temp = senzor.temperature()
+  var humid = senzor.humidity()
+  res.send({humid, temp});
 });
 
 module.exports = router;
